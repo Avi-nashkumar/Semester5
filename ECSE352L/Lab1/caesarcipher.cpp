2d7649ecfba08963e8f74a476532728325b51c97
Avinash Kumar <avinashrkmv@gmail.com>
2020-08-20 11:07:51 +0530

    Lab1 added

---
+++ ECSE352L/Lab1/caesarcipher.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string encryption(string text,int key){
+    int len_text= text.length();
+    string output="";
+    for (int i = 0; i < len_text; i++)
+    {
+        if(isupper(text[i])){
+            output+=char(int(text[i]+key-65)%26 +65);
+
+        }
+        else
+        {
+            output+= char(int(text[i]+key-97)%26 +97);
+        }
+        
+        
+    }
+    return output;
+    
+}
+string decryption(string text,int key){
+    int len_text= text.length();
+    string output="";
+    for (int i = 0; i < len_text; i++)
+    {
+        if(isupper(text[i])){
+            output+=char(int(text[i]-key-65)%26 +65);
+
+        }
+        else
+        {
+            output+= char(int(text[i]-key-97)%26 +97);
+        }
+        
+        
+    }
+    return output;
+    
+}
+
+int main(){
+    cout<<"Enter the message:"<<endl;
+    string text;
+    getline(cin, text);
+    int size;
+    cin>>size;
+    cout<<"1 for encryption and 2 for decryption"<<endl;
+    int i;
+    
+    for( i=1;i<=2;i++){
+        cin>>i;
+        cout<<"\n";
+        
+        
+        if(i==1){
+             cout<<encryption(text,size);
+        }
+        if(i==2) cout<<decryption(text,size);
+
+
+
+    }
+
+
+    
+
+
+
+
+    return 0;
+}
---
+++ ECSE352L/Lab1/random.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <algorithm>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h> 
+using namespace std;
+
+
+int main(){
+    for(int i=1;i<=10;i++){
+        cout<<(rand() % (10000))<<endl ;
+        cout<<"\n";
+
+    }
+
+
+    return 0;
+}
